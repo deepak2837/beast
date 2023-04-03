f3bb38d46c5b1808c9715bb313c58195fcea7970
Deepak <52418151+deepak2837@users.noreply.github.com>
2023-04-03 13:49:17 +0530

    Update emailClient.js

--- Client/emailClient.js
+++ Client/emailClient.js
@@ -17,8 +17,8 @@ export const sendEmail =(subject,content,ticketId,requester,receipientEmails) =>
         },
         headers: { "Content-Type": "application/json" }
     };
-console.log("args data ",args.data)
-    client.post("http://127.0.0.1:8001/crm/api/notification", args, (data, response) => {
+// console.log("args data ",args.data)
+//     client.post("http://127.0.0.1:8001/crm/api/notification", args, (data, response) => {
        
     console.log("request sent successfully");
     });
